2a0d1d7c8970a58d63c2ae9aac3a87ee7b4c74f9
MahdiBelguith <mahdibelguith9@gmail.com>
2025-09-29 20:04:42 +0100

    [--/MaBe]: add new changment

--- test.java
+++ test.java
@@ -1,4 +1,6 @@
-
-public class test {
-Print("hello")
+public class HelloWorld {
+    // main method is the entry point of any Java program
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
 }
