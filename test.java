c3d5a6213a6ee03b0200620b6902695b2348f17e
MahdiBelguith <mahdibelguith9@gmail.com>
2025-09-30 22:16:07 +0100

    [--/MaBe]: j'ai supprimer les fichier de test

--- test.java
+++
@@ -1,6 +0,0 @@
-public class HelloWorld {
-    // main method is the entry point of any Java program
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
-}
